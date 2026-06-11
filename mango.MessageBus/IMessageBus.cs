27bfdb1d8679d0586f393a330a99d17c5d54c9e9
praveen fuloria <praveenfuloriawipro@outlook.com>
2026-06-11 19:57:30 +0530

    Service Bus

---
+++ mango.MessageBus/IMessageBus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mango.MessageBus
+{
+    public interface IMessageBus
+    {
+        Task PublishMessage(object message, string topic_queue_Name);
+    }
+}
---
+++ mango.MessageBus/MessageBus.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mango.MessageBus
+{
+    internal class MessageBus : IMessageBus
+    {
+        private string connectionString = "";
+        public async Task PublishMessage(object message, string topic_queue_Name = "emailshoppingcart")
+        {
+            var client = new ServiceBusClient(connectionString); 
+            ServiceBusSender serviceBusSender = client.CreateSender(topic_queue_Name);
+
+            var jsonMessage = JsonConvert.SerializeObject(message);
+
+            ServiceBusMessage finalMessage = new ServiceBusMessage(Encoding.UTF8.GetBytes(jsonMessage))
+            {
+                CorrelationId = Guid.NewGuid().ToString()
+            };
+
+             await serviceBusSender.SendMessageAsync(finalMessage);
+            await client.DisposeAsync();
+
+        }
+    }
+}
